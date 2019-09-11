2cd894129be02af044f2daf439de09113f41f5ed
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 21:56:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   test_array = []
   array.collect do |string|
-    return_array << string.reverse
+    tets << string.reverse
   end
   return_array.join(" ")
 end
