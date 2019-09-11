2d34d33f6d4dc6d14efc762eb338bacdf1b1ee0b
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 21:06:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
 new_array = []
 i = 0
 string.each do [reverse]
-new_array << reverse.reverse_inplace
+new_array << reverse.reverse_inplace!
 end
 return new_array
 end
