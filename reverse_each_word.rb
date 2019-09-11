28915782e0dc742ccce1ec2407a5464a98542b11
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 20:47:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse(string)
  reversed_string = ''
 i = 0
- while i < string.length
-  reversed_string = string[i] + reversed_string
+while i < string.length
+reversed_string = string[i] + reversed_string
 i += 1
 end
 
