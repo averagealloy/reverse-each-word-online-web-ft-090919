a4f45d31300183c1d6a977c04a187bb6d53fc610
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 21:55:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word(string)
   array = string.split(" ")
   test_array = []
-  original_array.each do |string|
+  o= do |string|
     return_array << string.reverse
   end
   return_array.join(" ")
