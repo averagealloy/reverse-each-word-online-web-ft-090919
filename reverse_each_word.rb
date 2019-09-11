c0cbd1fb28ea401ee727aee52616d55ea84e1722
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 21:21:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,7 +14,7 @@ def reverse_each_word(string)
     new_array = []
     reverse_array = []
 
-    i = string.length
+i = string.length
 new_array.push(string.split(""))
 new_array.each do |item|
 until i < 0
